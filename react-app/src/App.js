9e902554466ca4fcf8cebf1db593eceee0c275c4
s-m-john <107650644+s-m-john@users.noreply.github.com>
2023-07-15 00:59:53 +0000

    Updates

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,31 +1,49 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-
-const backendHostUrl = 'https://s-m-john-fantastic-fortnight-p4jx6q9gvx726pq9-5001.preview.app.github.dev'
-
 function App() {
+  const[currTemp, setCurrTemp] = useState(0);
+  const[currConition, setCurrentCondition] = useState("")
+  const[dayLowTemp, setDayLowTemp] = useState(0);
+  const[dayHighTemp, setDayHighTemp] = useState(0);
+  const[WeatherDataByHourPages, setWeatherDataByHourPages]
   useEffect(() => {
     (async () => {
-      const res = await fetch(
-        `${backendHostUrl}/geeks-firebase-72e6d/us-central1/getDayWeather`,
-       
+      try {
+        const res = await fetch(
+        "http://127.0.0.1:5001/geeks-firebase-72e6d/us-central1/getDayWeather"      
       );
 
       const data = await res.json();
 
+      setCurrTemp(data.data.current.temp_f);
+      setCurrentCondition(data.data.current.condition.text);
+
+      setCurrentPage(data..data.currentPage);
+      // const currHour = data.data.hour.find((hour)) => hour.current);
+
+      setDayLowTemp(data.data.day.mintemp_f);
+      setDayHighTemp(data.data.day.max_temp_f);
       console.log("The res: ", data);
-    })();
-  }, []);
+    { catch (e) {
+      console.error(e.message);
+    }
+  } )();
+}, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      div className="current">
+        {!!currTemp && <h2 className="current-temp">{currTemp}° F</h2>}
+
+        {!!curCondition && (
+          <p className="current-condition">{currCondition}</p>
+        )}
+        <div className="low-high-temp">
+          <span></span>
+        </div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
